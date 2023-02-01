f1b20d4bf90a0054df2ab599d527099612e34472
Abdelmasieh01 <gokuabdo31@gmail.com>
2023-02-01 16:32:59 +0200

    Editing settings

--- records/settings.py
+++ records/settings.py
@@ -57,6 +57,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    #Whitenoise_Middleware
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'records.urls'
